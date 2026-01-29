534ad7c0220488b16cc9fab8f10e9888b29104e5
dleza <lezadanny@gmail.com>
2026-01-30 01:26:55 +0200

    setting module in build

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 
+export DJANGO_SETTINGS_MODULE=config.settings
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+
